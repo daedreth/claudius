2108c150572d3d33ed105cce953da753bcc9fd4b
rainyuu <gabriel2012688@gmail.com>
2017-11-02 14:24:20 +0100

    fix bug number uhhhhhhhhhhhhhhhhhhh
    fucjk

--- claudius.py
+++ claudius.py
@@ -63,3 +63,5 @@ async def on_message(message):
 if __name__ == '__main__':
     print('Connecting, please wait...')
     client.run(config['Main']['token'], bot=False)
+
+    #MADE BY RAI OBVIOUSLY
